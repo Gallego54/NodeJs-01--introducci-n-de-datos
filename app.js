8fb7a86124fb912ef9122d53574bbe04a842fdeb
Gallego54 <lolcuenta2000@gmail.com>
2022-06-12 14:11:06 -0300

    ''

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const http = require('http')
-const fs = require('fs')
+import http from 'http' 
+import fs from 'fs'
+import searchCSS from './src/modules/serverCSS.js'
+
 
 const PORT = 8000;
 
@@ -8,32 +10,45 @@ http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' })
 
     if ('/update' == req.url) {
+        const HTMLNOCSS = getHTML ('src/view/action.html');
+        const CSS = searchCSS ('./src/public/css/', 'style.css');
+        const HTML = insertCSS(HTMLNOCSS, CSS);
 
-        const HTML = getHTML ('src/view/action.html');
+
         GetParams(req, function(Params){
             const Pyramid = MakePyramid( Params.get('total') )
-            const  INDEX = HTML.search('body')+6;
+            const  INDEX = HTML.search('<body>')+'<body>'.length;
 
             //Total = addStr(HTML, INDEX, Pyramid);
 
-            return res.end(HTML.substring(0, INDEX)+
-            Pyramid+HTML.substring(INDEX, HTML.length))
+            return res.end(HTML.substring(0, INDEX)+'<div class="py">'
+            +Pyramid+'</div>'+HTML.substring(INDEX, HTML.length))
         });
 
         
         //return res.end(HTML);
 
     } else {
-
-        const HTML = getHTML ('src/view/form.html');
+        const HTMLNOCSS = getHTML ('src/view/form.html');
+        const CSS = searchCSS ('./src/public/css/', 'style.css');
+        const HTML = insertCSS(HTMLNOCSS, CSS);
         return res.end(HTML);
 
     }
 }).listen(PORT, _ => {
-
     console.log("PORT: " + PORT)
 })
 
+const insertCSS = (HTML, CSS) => {
+    const INDEX = HTML.search('<style>')+'<style>'.length;
+
+    if (!CSS) {
+        return HTML;
+    } 
+
+    return HTML.substring(0, INDEX)+CSS
+    +HTML.substring(INDEX, HTML.length);
+}
 
 const getHTML = PATH => {
     return fs.readFileSync(PATH, 
@@ -62,7 +77,7 @@ const MakePyramid = (  MAX ) => {
    
     let HTMLAux = '';
 
-    for (let i=0 ; i <=  MAX ; i++) {
+    for (let i=0 ; i <  MAX ; i++) {
         for (let y=0 ; y<=MAX-i ; y++) {
             HTMLAux += '&nbsp;&nbsp;';
         }
